e66f08f7a22af2ea4b3cefb131b93df843dbcf4a
Ayesha Ansari <itsa.ansari@gmail.com>
2025-11-13 02:56:04 +0500

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='items-center justify-items-center font-bold text-9xl'>FinSight AI</h1>
+    </>
+  )
+}
+
+export default App
